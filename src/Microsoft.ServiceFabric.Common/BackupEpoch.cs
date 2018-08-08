437868cf09a00a2a3da72a229b174e677434b93b
Aman Bhardwaj <amanbha@users.noreply.github.com>
2018-08-07 17:24:42 -0700

    Adding an .editorconfig and stylecop fizes (#29)

---
+++ src/Microsoft.ServiceFabric.Common/BackupEpoch.cs
@@ -0,0 +1,47 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace Microsoft.ServiceFabric.Common
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// An Epoch is a configuration number for the partition as a whole. When the configuration of the replica set changes,
+    /// for example when the Primary replica changes, the operations that are replicated from the new Primary replica are
+    /// said to be a new Epoch from the ones which were sent by the old Primary replica.
+    /// </summary>
+    public partial class BackupEpoch
+    {
+        /// <summary>
+        /// Initializes a new instance of the BackupEpoch class.
+        /// </summary>
+        /// <param name="configurationNumber">The current configuration number of this Epoch. The configuration number is an
+        /// increasing value that is updated whenever the configuration of this replica set changes.</param>
+        /// <param name="dataLossNumber">The current dataloss number of this Epoch. The data loss number property is an
+        /// increasing value which is updated whenever data loss is suspected, as when loss of a quorum of replicas in the
+        /// replica set that includes the Primary replica.</param>
+        public BackupEpoch(
+            string configurationNumber = default(string),
+            string dataLossNumber = default(string))
+        {
+            this.ConfigurationNumber = configurationNumber;
+            this.DataLossNumber = dataLossNumber;
+        }
+
+        /// <summary>
+        /// Gets the current configuration number of this Epoch. The configuration number is an increasing value that is
+        /// updated whenever the configuration of this replica set changes.
+        /// </summary>
+        public string ConfigurationNumber { get; }
+
+        /// <summary>
+        /// Gets the current dataloss number of this Epoch. The data loss number property is an increasing value which is
+        /// updated whenever data loss is suspected, as when loss of a quorum of replicas in the replica set that includes the
+        /// Primary replica.
+        /// </summary>
+        public string DataLossNumber { get; }
+    }
+}
