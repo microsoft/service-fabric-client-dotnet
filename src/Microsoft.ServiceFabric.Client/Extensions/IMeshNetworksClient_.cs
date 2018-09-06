ba5334a6081726faed968d0a5da993ee7841cb5c
Aman Bhardwaj <amanbha@microsoft.com>
2018-09-05 18:02:56 -0700

    Updating Network apis.

--- src/Microsoft.ServiceFabric.Client/Extensions/IMeshNetworksClient_.cs
+++ src/Microsoft.ServiceFabric.Client/Extensions/IMeshNetworksClient_.cs
@@ -38,7 +38,7 @@ namespace Microsoft.ServiceFabric.Client
         /// <exception cref="ServiceFabricRequestException">Thrown when request to Service Fabric cluster failed due to an underlying issue such as network connectivity, DNS failure or timeout.</exception>
         /// <exception cref="ServiceFabricException">Thrown when the requested operation failed at server. Exception contains Error code <see cref="FabricError.ErrorCode"/>, message indicating the failure. It also contains a flag wether the exception is transient or not, client operations can be retried if its transient.</exception>
         /// <exception cref="OperationCanceledException">Thrown when cancellation is requested for the cancellation token.</exception>
-        Task<VolumeResourceDescription> CreateOrUpdateMeshNetworkAsync(
+        Task<NetworkDescription> CreateOrUpdateMeshNetworkAsync(
             string networkResourceName,
             string jsonDescription,
             string apiVersion = Constants.DefaultApiVersionForResources,
