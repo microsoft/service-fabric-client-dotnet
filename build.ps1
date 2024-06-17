d850c738ace9517a89ff9ba4bb1d3053d2d47ad6
Goran Obradovic <gobradovic@microsoft.com>
2024-06-17 11:17:09 +0200

    Use MSBuild from VS2022 instead of VS2019

--- build.ps1
+++ build.ps1
@@ -12,7 +12,7 @@ param
     #Options are:
     #   RebuildAll: Clean, Build all .csproj and Generate Nuget Packages. This is the default option.
     #   BuildAll: Build all .csproj and Generate Nuget Packages.
-	#   BuildCode: Builds code, doesn't generates nuget packages.
+    #   BuildCode: Builds code, doesn't generates nuget packages.
     #   GeneratePackages: Generates nuget packages, this target doesn't builds code, build must be done using BuildCode target before invoking this target.
     [ValidateSet('Rebuildall', 'BuildAll', 'BuildCode', 'GeneratePackages')]
     [string]$Target = "RebuildAll",
@@ -33,36 +33,31 @@ if($MSBuildFullPath -ne "")
     }
 }
 
-# msbuild path not provided, find msbuild for VS2019
+# msbuild path not provided, find msbuild for VS2022
 if($MSBuildFullPath -eq "")
 {
-    if (Test-Path "env:\ProgramFiles(x86)")
+    $progFilesPaths = ${env:\ProgramFiles(x86)}, ${env:\ProgramFiles}
+    foreach ($progFilesPath in $progFilesPaths)
     {
-        $progFilesPath =  ${env:ProgramFiles(x86)}
-    }
-    elseif (Test-Path "env:\ProgramFiles")
-    {
-        $progFilesPath =  ${env:ProgramFiles}
-    }
+        $VS2022InstallPath = join-path $progFilesPath "Microsoft Visual Studio\2022"
+        $versions = 'Preview', 'Community', 'Professional', 'Enterprise'
 
-    $VS2019InstallPath = join-path $progFilesPath "Microsoft Visual Studio\2019"
-    $versions = 'Community', 'Professional', 'Enterprise'
-
-    foreach ($version in $versions)
-    {
-        $VS2019VersionPath = join-path $VS2019InstallPath $version
-        $MSBuildFullPath = join-path $VS2019VersionPath "MSBuild\Current\Bin\MSBuild.exe"
-
-        if (Test-Path $MSBuildFullPath)
+        foreach ($version in $versions)
         {
-            break
+            $VS2022VersionPath = join-path $VS2022InstallPath $version
+            $testPath = join-path $VS2022VersionPath "MSBuild\Current\Bin\MSBuild.exe"
+
+            if (Test-Path $testPath)
+            {
+                $MSBuildFullPath = $testPath
+            }
         }
     }
 }
 
-if (!(Test-Path $MSBuildFullPath))
+if (($MSBuildFullPath -eq "") -or !(Test-Path $MSBuildFullPath))
 {
-    throw "Unable to find MSBuild installed on this machine. Please install Visual Studio 2019 or if its installed at non-default location, provide the full ppath to msbuild using -MSBuildFullPath parameter."
+    throw "Unable to find MSBuild installed on this machine. Please install Visual Studio 2022 or if its installed at non-default location, provide the full path to msbuild using -MSBuildFullPath parameter."
 }
 
 
